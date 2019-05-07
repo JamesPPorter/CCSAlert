c22cbf270d49a0cd0c0b58af61d609ff606b4548
James Porter <jpporter04@icloud.com>
2019-05-07 15:41:31 -0400

    Login and Sign Up pages are functioning

--- CHSAlert/CHSAlert/views.py
+++ CHSAlert/CHSAlert/views.py
@@ -20,6 +20,6 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
+    return render(request, 'ChatRoom.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
